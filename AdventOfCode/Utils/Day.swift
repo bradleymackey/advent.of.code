fbc436287449e2e9542f3341cceab560b21c0271
Bradley Mackey <bradleymackey@me.com>
2019-12-02 00:41:57 +0000

    break out day solver to seperate file

--- AdventOfCode/Utils/Day.swift
+++ AdventOfCode/Utils/Day.swift
@@ -13,34 +13,10 @@ protocol Day {
 }
 
 extension Day {
+    
     func solveAll() {
         print("Part 1: \(solvePartOne())")
         print("Part 2: \(solvePartTwo())")
-    }
-}
-
-enum DaySolveError: Error {
-    case dayNotFound
-    var localizedDescription: String {
-        switch self {
-        case .dayNotFound:
-            return "Day not found."
-        }
-    }
-}
-
-struct DaySolver {
-    
-    let days: [Int: Day.Type] = [
-        1: Day1.self,
-    ]
-    
-    func solveAll(for day: Int, providing input: String) throws {
-        if let selectedDay = days[day] {
-            selectedDay.init(input: input).solveAll()
-        } else {
-            throw DaySolveError.dayNotFound
-        }
     }
     
 }
---
+++ AdventOfCode/Utils/DaySolver.swift
@@ -0,0 +1,32 @@
+//
+//  DaySolver.swift
+//  AdventOfCode
+//
+//  Created by Bradley Mackey on 02/12/2019.
+//  Copyright © 2019 Bradley Mackey. All rights reserved.
+//
+
+enum DaySolveError: Error {
+    case dayNotFound
+    var localizedDescription: String {
+        switch self {
+        case .dayNotFound:
+            return "Day not found."
+        }
+    }
+}
+
+struct DaySolver {
+    
+    let days: [Int: Day.Type] = [
+        1: Day1.self,
+    ]
+    
+    func solveAll(for day: Int, providing input: String) throws {
+        guard let selectedDay = days[day] else {
+            throw DaySolveError.dayNotFound
+        }
+        selectedDay.init(input: input).solveAll()
+    }
+    
+}
