9b2c08a8ee5a1c5a88f5ebc54122f0a42a4371fd
Bradley Mackey <bradleymackey@me.com>
2019-12-02 15:09:57 +0000

    complete rename of test target

--- Tests/AdventOfCode19Tests/AdventOfCode19Tests.swift
+++ Tests/AdventOfCode19Tests/AdventOfCode19Tests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import class Foundation.Bundle
 
-final class advent_of_code_19Tests: XCTestCase {
+final class AdventOfCode19Tests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
@@ -12,7 +12,7 @@ final class advent_of_code_19Tests: XCTestCase {
             return
         }
 
-        let fooBinary = productsDirectory.appendingPathComponent("advent-of-code-19")
+        let fooBinary = productsDirectory.appendingPathComponent("AdventOfCode19")
 
         let process = Process()
         process.executableURL = fooBinary
--- Tests/AdventOfCode19Tests/XCTestManifests.swift
+++ Tests/AdventOfCode19Tests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(advent_of_code_19Tests.allTests),
+        testCase(AdventOfCode19Tests.allTests),
     ]
 }
 #endif
