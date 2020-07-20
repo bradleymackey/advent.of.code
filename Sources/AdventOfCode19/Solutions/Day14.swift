3da35cc9e898bff1aee9423f880255e043418722
Bradley Mackey <bradleymackey@me.com>
2020-07-20 21:58:42 +0100

    add day 14

---
+++ Sources/AdventOfCode19/Solutions/Day14.swift
@@ -0,0 +1,25 @@
+//
+//  Day14.swift
+//  
+//
+//  Created by Bradley Mackey on 20/07/2020.
+//
+
+/// --- Day 14: Space Stoichiometry ---
+final class Day14: Day {
+    
+    let input: String
+    
+    init(input: String) {
+        self.input = input
+    }
+    
+    func solvePartOne() -> CustomStringConvertible {
+        "?"
+    }
+    
+    func solvePartTwo() -> CustomStringConvertible {
+        "?"
+    }
+    
+}
--- Sources/AdventOfCode19/Utils/Resolver.swift
+++ Sources/AdventOfCode19/Utils/Resolver.swift
@@ -35,7 +35,7 @@ struct Resolver {
         11: Day11.self,
         12: Day12.self,
         13: Day13.self,
-//        14: Day14.self,
+        14: Day14.self,
 //        15: Day15.self,
 //        16: Day16.self,
 //        17: Day17.self,
