c0a475c4658cc0686d1a86d3191ef816e6061ecc
Bradley Mackey <bradleymackey@me.com>
2020-07-25 15:32:48 +0100

    faster maze parsing

--- Sources/AdventOfCode19/Solutions/Day20.swift
+++ Sources/AdventOfCode19/Solutions/Day20.swift
@@ -48,25 +48,15 @@ extension Day20 {
         let map: String
         private var outerXLarge: ClosedRange<Int>!
         private var outerYLarge: ClosedRange<Int>!
-        private lazy var lines = map.split(separator: "\n")
         
-        /// indexes correspond to coordinates, for fast random access
-        private lazy var charsArray = lines.lazy.compactMap {
-            line -> [Character] in
-            line.unicodeScalars.map(Character.init) + "\n"
-        }
-        
-        /// characters in a single list for iterating
-        private lazy var chars = lines.lazy.flatMap {
-            line -> [Character] in
-            line.unicodeScalars.map(Character.init) + "\n"
-        }
+        private let chars: [[Character]]
         
         init(map: String) {
             self.map = map
-            let maxX = lines.first?.count ?? Int.max
+            self.chars = map.split(separator: "\n").map(String.init).map { Array($0) }
+            let maxX = chars.first?.count ?? Int.max
             self.outerXLarge = (maxX - 10)...maxX
-            let maxY = lines.count
+            let maxY = chars.count
             self.outerYLarge = (maxY - 10)...maxY
         }
         
@@ -80,45 +70,37 @@ extension Day20 {
             var features = [Vector2: Object]()
             var portalEndpoints = [String: Object.Portal.Endpoint]()
             var coor: Vector2 = .zero
-            for char in chars {
-                
-                guard char != "\n" else {
-                    coor.y += 1
-                    coor.x = 0
-                    continue
-                }
-                
-                defer {
-                    coor.x += 1
-                }
-                
-                guard let rawObject = RawObject(rawValue: char) else { continue }
-                switch rawObject {
-                case .wall:
-                    features[coor] = .wall
-                case .path:
-                    guard let (id, endpoint) = portalEndpoint(for: coor) else {
-                        features[coor] = .path(portal: nil); break
+            for line in chars {
+                defer { coor.y += 1; coor.x = 0 }
+                for char in line {
+                    defer { coor.x += 1 }
+                    guard let rawObject = RawObject(rawValue: char) else { continue }
+                    switch rawObject {
+                    case .wall:
+                        features[coor] = .wall
+                    case .path:
+                        guard let (id, endpoint) = portalEndpoint(for: coor) else {
+                            features[coor] = .path(portal: nil); break
+                        }
+                        if let unconnected = portalEndpoints[id] {
+                            assert(
+                                unconnected.position != endpoint.position,
+                                ""
+                            )
+                            let portal = Object.Portal.complete(id: id, entry: unconnected, exit: endpoint)
+                            features[unconnected.adjacentPath] = .path(portal: portal)
+                            features[endpoint.adjacentPath] = .path(portal: portal.flipped())
+                            portalEndpoints[id] = nil
+                        } else {
+                            features[endpoint.adjacentPath] = .path(portal: .incomplete(id: id, endpoint))
+                            portalEndpoints[id] = endpoint
+                        }
+                    case .letter, .empty:
+                        // letters for portals are dealt with in the `.path` code, because we need to find
+                        // the paths that portals are attached to for them to make sense
+                        break
                     }
-                    if let unconnected = portalEndpoints[id] {
-                        assert(
-                            unconnected.position != endpoint.position,
-                            ""
-                        )
-                        let portal = Object.Portal.complete(id: id, entry: unconnected, exit: endpoint)
-                        features[unconnected.adjacentPath] = .path(portal: portal)
-                        features[endpoint.adjacentPath] = .path(portal: portal.flipped())
-                        portalEndpoints[id] = nil
-                    } else {
-                        features[endpoint.adjacentPath] = .path(portal: .incomplete(id: id, endpoint))
-                        portalEndpoints[id] = endpoint
-                    }
-                case .letter, .empty:
-                    // letters for portals are dealt with in the `.path` code, because we need to find
-                    // the paths that portals are attached to for them to make sense
-                    break
                 }
-                
             }
             
             let start = portalEndpoints["AA"]!.adjacentPath
@@ -128,7 +110,7 @@ extension Day20 {
         }
         
         private func portalEndpoint(for coor: Vector2) -> (id: String, endpoint: Object.Portal.Endpoint)? {
-            guard let rawObject = RawObject(rawValue: charsArray[coor.y][coor.x]) else { return nil }
+            guard let rawObject = RawObject(rawValue: chars[coor.y][coor.x]) else { return nil }
             guard case .path = rawObject else { return nil }
             directions: for direction in Direction.allCases {
                 var portalLabel = ""
@@ -139,7 +121,7 @@ extension Day20 {
                     defer {
                         pointer = direction.moving(pointer)
                     }
-                    let object = RawObject(rawValue: charsArray[pointer.y][pointer.x])
+                    let object = RawObject(rawValue: chars[pointer.y][pointer.x])
                     guard case .letter(let chr) = object else {
                         if portalLabel.isEmpty {
                             continue directions
@@ -343,7 +325,7 @@ extension Day20.Maze {
         var targetDistance = Int.max
         
         while let (current, dist) = explore.dequeue() {
-            
+        
             if current.xy == end.xy { print("  - D=\(current.z):", dist) }
             if current == end { targetDistance = dist; break }
             if seen.contains(current) { continue }
