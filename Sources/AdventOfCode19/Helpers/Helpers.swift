77a5ef685f87f39416749b2460618ef66bdf1dde
Bradley Mackey <bradleymackey@me.com>
2020-07-29 21:26:51 +0100

    move more algs to shared folder

--- Sources/AdventOfCode19/Helpers/Helpers.swift
+++ Sources/AdventOfCode19/Helpers/Helpers.swift
@@ -31,32 +31,62 @@ extension Sequence where Element == Int {
     
 }
 
-/// https://sveinhal.github.io/2018/06/14/repeating-sequence/
-public struct RepeatingSequence<T: Collection>: Sequence {
-    
-    private let base: T
-    
-    public init(baseCollection: T) {
-        self.base = baseCollection
-    }
-    
-    public func makeIterator() -> AnyIterator<T.Element> {
-        var i = base.startIndex
-        return AnyIterator({ [base] () -> T.Element in
-            defer {
-                i = (base.index(after: i) == base.endIndex)
-                    ? base.startIndex : base.index(after: i)
-            }
-            return base[i]
-        })
-    }
-    
-}
+// MARK: - Permute
 
 extension Collection {
     
-    public func repeated() -> RepeatingSequence<Self> {
-        RepeatingSequence(baseCollection: self)
+    /// no native permute in swift, we this this implementation
+    /// algorithm from: https://stackoverflow.com/a/34969388/3261161
+    func permute() -> [[Iterator.Element]] {
+        var scratch = Array(self) // This is a scratch space for Heap's algorithm
+        var result: [[Iterator.Element]] = [] // This will accumulate our result
+        
+        // Heap's algorithm
+        func heap(_ n: Int) {
+            if n == 1 {
+                return result.append(scratch)
+            }
+            
+            for i in 0..<n-1 {
+                heap(n-1)
+                let j = (n%2 == 1) ? 0 : i
+                scratch.swapAt(j, n-1)
+            }
+            heap(n-1)
+        }
+        
+        // Let's get started
+        heap(scratch.count)
+        
+        // And return the result we built up
+        return result
+    }
+    
+    /// https://stackoverflow.com/a/51474264/3261161
+    func combinations(takenBy: Int) -> [[Element]] {
+        if count == takenBy {
+            return [Array(self)]
+        }
+        
+        if isEmpty {
+            return []
+        }
+        
+        if takenBy == 0 {
+            return []
+        }
+        
+        if takenBy == 1 {
+            return map { [$0] }
+        }
+        
+        var result: [[Element]] = []
+        
+        let rest = self[index(after: startIndex)...]
+        let subCombos = rest.combinations(takenBy: takenBy - 1)
+        result += subCombos.map { [self[startIndex]] + $0 }
+        result += rest.combinations(takenBy: takenBy)
+        return result
     }
     
 }
