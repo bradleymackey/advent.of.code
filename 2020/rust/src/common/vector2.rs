d181130ea275430a261317893dc75fbbe84cd4e2
Bradley Mackey <bradleymackey@me.com>
2020-12-06 21:09:06 +0000

    x,y labels in vector 2 printout

--- 2020/rust/src/common/vector2.rs
+++ 2020/rust/src/common/vector2.rs
@@ -101,7 +101,7 @@ where
     N: Num + Ord + Display + Copy,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.x, self.y)
+        write!(f, "(x:{}, y:{})", self.x, self.y)
     }
 }
 
