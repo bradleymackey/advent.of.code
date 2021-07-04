cb0db10144ee7e2a197aa6b0a01c3520edf5fa34
Bradley Mackey <bradleymackey@me.com>
2021-07-04 17:17:58 +0100

    day 9: use i32, remove magic number part 2

--- 2020/rust/src/solutions/day09.rs
+++ 2020/rust/src/solutions/day09.rs
@@ -1,13 +1,14 @@
 // --- Day 9: Encoding Error ---
 
 #[aoc_generator(day9)]
-fn parse_input(input: &str) -> Vec<i64> {
+fn parse_input(input: &str) -> Vec<i32> {
     input.lines().filter_map(|l| l.parse().ok()).collect()
 }
 
+/// For part 1, the look-back size.
 const WINDOW_SIZE: usize = 25;
 
-fn invalid_number(input: &Vec<i64>) -> Option<i64> {
+fn invalid_number(input: &Vec<i32>) -> Option<i32> {
     'i: for i in WINDOW_SIZE..input.len() {
         let current = input[i];
         for j in (i - WINDOW_SIZE)..=i {
@@ -26,18 +27,21 @@ fn invalid_number(input: &Vec<i64>) -> Option<i64> {
 }
 
 #[aoc(day9, part1)]
-fn part1(input: &Vec<i64>) -> Option<i64> {
+fn part1(input: &Vec<i32>) -> Option<i32> {
     invalid_number(input)
 }
 
+/// For part 2. The minimum number of contiguous elements that make up the result.
+const MIN_CONTIGUOUS_LEN: usize = 2;
+
 #[aoc(day9, part2)]
-fn part2(input: &Vec<i64>) -> Option<i64> {
+fn part2(input: &Vec<i32>) -> Option<i32> {
     let target_number = invalid_number(input)?;
-    for size in 2..input.len() {
+    for size in MIN_CONTIGUOUS_LEN..input.len() {
         for upper in size..input.len() {
             let lower = upper - size;
             let slice = &input[lower..upper];
-            if slice.iter().sum::<i64>() == target_number {
+            if slice.iter().sum::<i32>() == target_number {
                 let min = slice.iter().min()?;
                 let max = slice.iter().max()?;
                 return Some(min + max);
