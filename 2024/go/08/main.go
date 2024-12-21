bf250846b3df7e6181d4f0dbea9d96b85ffe4952
Bradley Mackey <bradleymackey@me.com>
2024-12-21 17:39:29 +0000

    Create template

---
+++ 2024/go/08/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("empty input.txt file")
+	}
+}
+
+func main() {
+	ans1 := Part1(input)
+	fmt.Println("Part 1:", ans1)
+	ans2 := Part2(input)
+	fmt.Println("Part 2:", ans2)
+}
+
+func Part1(input string) int {
+	return 0
+}
+
+func Part2(input string) int {
+	return 0
+}
